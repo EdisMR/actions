b3245973e13b36a519434a56193384b641bae296
EdisAnthony <edisanthony@gmail.com>
2021-04-06 22:15:45 -0600

    Reestructurando index completo

--- js/main.js
+++ js/main.js
@@ -5,25 +5,13 @@
 window.addEventListener("mousemove",mouseMovement,false)
 function mouseMovement(e){
 	let evento=e;
-	let innerMouse=Array.from(document.querySelectorAll(".events .inner-info"))[0];
+	let innerMouse=Array.from(document.querySelectorAll(".header-events .inner-event"))[0];
 	innerMouse.innerHTML="X="+parseInt(evento.clientX)+", Y="+parseInt(evento.clientY);
-	watchEventsTarget()
 }
 
 window.addEventListener("keyup",keyEvent,false);
 function keyEvent(e){
 	evento=e;
-	let innerKey=Array.from(document.querySelectorAll(".events .inner-info"))[1];
+	let innerKey=Array.from(document.querySelectorAll(".header-events .inner-event"))[1];
 	innerKey.innerHTML='"'+evento.key+'"';
-	watchEventsTarget()
-}
-
-function watchEventsTarget(){
-	let estilo=window.getComputedStyle(divEventos,null).getPropertyValue("display");
-	if(estilo=="none"){
-		divEventos.style.display="block";
-		setTimeout(function (){
-			divEventos.style.display="none";
-		},3000)
-	}
 }
