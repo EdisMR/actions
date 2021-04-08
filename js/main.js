17cb95076923c72c15fb4291a0ac6e4240795ac6
EdisAnthony <edisanthony@gmail.com>
2021-04-07 19:59:15 -0600

    Menu superior funcional terminado

--- js/main.js
+++ js/main.js
@@ -15,3 +15,65 @@ function keyEvent(e){
 	let innerKey=Array.from(document.querySelectorAll(".header-events .inner-event"))[1];
 	innerKey.innerHTML='"'+evento.key+'"';
 }
+
+
+/* ****************************************** */
+/* ***** FUNCIONAMIENTO DE BOTON MENU ***** */
+/* ***************************************** */
+var headerButton={
+	btn:null,
+	disp:false,
+	headerEnlaces:null,
+	headerLi:null,
+};
+
+if(screen.orientation.type=="portrait-primary"){
+	/* Variables */
+	headerButton.btn=document.querySelector(".header-title button")
+	headerButton.headerEnlaces=document.querySelector(".header-enlaces");
+	headerButton.headerLi=Array.from(document.querySelectorAll(".header-enlaces a"));
+
+	headerButton.btn.addEventListener("click",menuSwitchMain,false);
+}
+
+function menuSwitchMain(){
+	/* Mostrar menu */
+	if(headerButton.disp==false){
+		headerButton.disp=true;
+
+		headerButton.headerEnlaces.style.display="block";
+
+
+		/* ANIMACION */
+		headerButton.headerLi.forEach(elm=>{
+			elm.style.display="flex";
+			elm.style.opacity="0%";
+			elm.addEventListener("animationend",function(){elm.style.opacity="100%";},false);
+		})
+
+		for(let x=0;x< headerButton.headerLi.length;x++){
+			headerButton.headerLi[x].classList.add("headerLiAnimated");
+			headerButton.headerLi[x].style.animationDelay=x+"00ms";
+		}
+		
+		headerButton.btn.innerHTML="&#x25B2;";
+		
+		
+	}else /* Ocultar menu */
+	
+	
+	if(headerButton.disp==true){
+		headerButton.disp=false;
+		
+		headerButton.headerEnlaces.style.display="none";
+		headerButton.headerLi.forEach(elm=>{
+			elm.style.display="none";
+			elm.classList.remove("headerLiAnimated");
+		})
+		headerButton.btn.innerHTML="&#x25BC;";
+	}
+}
+
+/* ****************************************** */
+/* ****************************************** */
+/* ***************************************** */
