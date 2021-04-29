27e5ae6b8984c8049171cfe49a4ba34d6b049854
EdisAnthony <edisanthony@gmail.com>
2021-04-28 21:41:02 -0600

    nada

--- js/api.js
+++ js/api.js
@@ -1,6 +1,10 @@
 const apiKey="b96ba3199b2f0a04e4eaec52229ff2b1"
-const urlSolicitud="https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid="+apiKey;
-var datos;
+const urlSolicitud="https://api.openweathermap.org/data/2.5/weather?lat=9.935243&lon=-84.11944&appid="+apiKey+"&lang=sp";
+
+var datosCapture;
+var datos={
+	weatherImagen:"",
+};
 
 fetch(
 	urlSolicitud,
@@ -13,6 +17,16 @@ fetch(
 	}
 ).then(
 	function(resp){
-		console.log(resp);
+		datosCapture=resp;
 	}
 )
+
+
+function dataFilter(){
+	datos.weatherImagen="http://openweathermap.org/img/wn/"+datosCapture.weather[0].icon+"@2x.png";
+	document.querySelector("main").innerHTML=`
+		<p>${datosCapture.weather[0].description}</p>
+		<img width="90%" height="auto" src="${datos.weatherImagen}">
+		</img>
+	`
+}
